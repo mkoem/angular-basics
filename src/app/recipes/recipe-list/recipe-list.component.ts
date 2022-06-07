0a749b2cbefe60375be29a18c85014351cb79c59
mkoem <106806703+mkoem@users.noreply.github.com>
2022-06-07 16:03:51 +0300

    Section3Course Project-The Basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe','This is simply a test','https://loseweightbyeating.com/wp-content/uploads/2021/03/AdobeStock_318489431-scaled.jpeg?ezimgfmt=ng:webp/ngcb1'),
+    new Recipe('A Test Recipe 2','This is simply a test 2','https://loseweightbyeating.com/wp-content/uploads/2021/03/AdobeStock_318489431-scaled.jpeg?ezimgfmt=ng:webp/ngcb1')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
